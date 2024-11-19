831190b8d0dd7aec5c55673fea185f827436c790
조성호 <newfuring1123@gmail.com>
2024-11-19 20:48:30 +0900

    Feat: Vite로 빌드 툴 변경 (#20)

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -1,9 +1,9 @@
 import { IEnvironment } from "@/types/common/environment";
 
-const VUE_ENV_PREFIX = "VUE_APP_";
+const VUE_ENV_PREFIX = "VITE_";
 
 export const getProperty = (key: keyof IEnvironment): string => {
-  const value = process.env[VUE_ENV_PREFIX + key];
+  const value = import.meta.env[`${VUE_ENV_PREFIX}${key}`];
   if (!value) {
     throw new Error(`Environment property ${key} is not set`);
   }
